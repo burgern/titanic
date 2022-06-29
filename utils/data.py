ad8299245b03ef842dff695522ee1eb7b3a7ea03
Nico Burger <nicoburger1998@gmail.com>
2022-06-30 00:18:39 +0200

    [setup] fix load_data() bug

--- utils/data.py
+++ utils/data.py
@@ -5,5 +5,5 @@ from config import ROOT_DIR
 
 
 def load_data(split: str = 'train'):
-    data_path = os.path.join(ROOT_DIR, split)
+    data_path = os.path.join(ROOT_DIR, 'data', split + ".csv")
     return pd.read_csv(data_path)
